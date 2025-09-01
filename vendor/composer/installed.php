76b772b2ede006bc496873fef9a9ab4026857aec
Farzad Hoseinzade <fstarlike@gmail.com>
2025-09-02 00:17:48 +0330

    Update LinkedIn profile link in the footer for improved accessibility

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-git-manager/plugin',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e9a280f8d60f45157713c133af98aa5d830fa2e5',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wp-git-manager/plugin' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e9a280f8d60f45157713c133af98aa5d830fa2e5',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
